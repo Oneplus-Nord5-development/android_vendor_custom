afb5a2df28aede7ea751ed44bf9a40e4a35eb466
grep <grepfox@tutamail.com>
2025-09-21 00:27:13 +0530

    custom: Dont build few of our additions for now
    Change-Id: I32ff044962f2c7130acdc15c85ca22b633cf77c3

--- build.mk
+++ build.mk
@@ -9,21 +9,21 @@ PRODUCT_PACKAGES += \
 	PrivacyBrowserFdroid \
 
 # Packages
-PRODUCT_PACKAGES += \
-    LMOFreeform \
-    LMOFreeformSidebar \
+# PRODUCT_PACKAGES += \
+#     LMOFreeform \
+#     LMOFreeformSidebar \
 
-ifeq ($(TARGET_WITH_GAMESPACE), true)
-PRODUCT_PACKAGES += GameSpace
-endif
+# ifeq ($(TARGET_WITH_GAMESPACE), true)
+# PRODUCT_PACKAGES += GameSpace
+# endif
 
-# Fonts
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/custom/prebuilts/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+# # Fonts
+# PRODUCT_COPY_FILES += \
+#     $(call find-copy-subdir-files,*,vendor/custom/prebuilts/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
 
-# Overlays
-PRODUCT_PACKAGES += \
-    FontGoogleSansFlexOverlay
+# # Overlays
+# PRODUCT_PACKAGES += \
+#     FontGoogleSansFlexOverlay
 
 # Lockscreenclocks
 PRODUCT_PACKAGES += \
@@ -37,70 +37,70 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Weather
 
 # Icon Packs
-PRODUCT_PACKAGES += \
-    IconPackCircularAndroidOverlay \
-    IconPackCircularLauncherOverlay \
-    IconPackCircularSettingsOverlay \
-    IconPackCircularSystemUIOverlay \
-    IconPackCircularThemePickerOverlay \
-    IconPackVictorAndroidOverlay \
-    IconPackVictorLauncherOverlay \
-    IconPackVictorSettingsOverlay \
-    IconPackVictorSystemUIOverlay \
-    IconPackVictorThemePickerOverlay \
-    IconPackSamAndroidOverlay \
-    IconPackSamLauncherOverlay \
-    IconPackSamSettingsOverlay \
-    IconPackSamSystemUIOverlay \
-    IconPackSamThemePickerOverlay \
-    IconPackKaiAndroidOverlay \
-    IconPackKaiLauncherOverlay \
-    IconPackKaiSettingsOverlay \
-    IconPackKaiSystemUIOverlay \
-    IconPackKaiThemePickerOverlay \
-    IconPackFilledAndroidOverlay \
-    IconPackFilledLauncherOverlay \
-    IconPackFilledSettingsOverlay \
-    IconPackFilledSystemUIOverlay \
-    IconPackFilledThemePickerOverlay \
-    IconPackPUIAndroidOverlay \
-    IconPackPUILauncherOverlay \
-    IconPackPUISettingsOverlay \
-    IconPackPUISystemUIOverlay \
-    IconPackPUIThemePickerOverlay \
-    IconPackRoundedAndroidOverlay \
-    IconPackRoundedLauncherOverlay \
-    IconPackRoundedSettingsOverlay \
-    IconPackRoundedSystemUIOverlay \
-    IconPackRoundedThemePickerOverlay \
-    IconPackOOSAndroidOverlay \
-    IconPackOOSLauncherOverlay \
-    IconPackOOSSettingsOverlay \
-    IconPackOOSSystemUIOverlay \
-    IconPackOOSThemePickerOverlay \
-    IconPackOutlineAndroidOverlay \
-    IconPackOutlineLauncherOverlay \
-    IconPackOutlineSettingsOverlay \
-    IconPackOutlineSystemUIOverlay \
-    IconPackAcherusAndroidOverlay \
-    IconPackAcherusLauncherOverlay \
-    IconPackAcherusSettingsOverlay \
-    IconPackAcherusSystemUIOverlay \
-    IconPackAuroraAndroidOverlay \
-    IconPackAuroraSystemUIOverlay \
-    IconPackGradiconAndroidOverlay \
-    IconPackGradiconSystemUIOverlay \
-    IconPackLornAndroidOverlay \
-    IconPackLornSystemUIOverlay \
-    IconPackPlumpySystemUIOverlay \
-    IconPackPlumpyAndroidOverlay \
-    IconPackXperiaAndroidOverlay \
-    IconPackXperiaSettingsOverlay \
-    IconPackXperiaSystemUIOverlay
+# PRODUCT_PACKAGES += \
+#     IconPackCircularAndroidOverlay \
+#     IconPackCircularLauncherOverlay \
+#     IconPackCircularSettingsOverlay \
+#     IconPackCircularSystemUIOverlay \
+#     IconPackCircularThemePickerOverlay \
+#     IconPackVictorAndroidOverlay \
+#     IconPackVictorLauncherOverlay \
+#     IconPackVictorSettingsOverlay \
+#     IconPackVictorSystemUIOverlay \
+#     IconPackVictorThemePickerOverlay \
+#     IconPackSamAndroidOverlay \
+#     IconPackSamLauncherOverlay \
+#     IconPackSamSettingsOverlay \
+#     IconPackSamSystemUIOverlay \
+#     IconPackSamThemePickerOverlay \
+#     IconPackKaiAndroidOverlay \
+#     IconPackKaiLauncherOverlay \
+#     IconPackKaiSettingsOverlay \
+#     IconPackKaiSystemUIOverlay \
+#     IconPackKaiThemePickerOverlay \
+#     IconPackFilledAndroidOverlay \
+#     IconPackFilledLauncherOverlay \
+#     IconPackFilledSettingsOverlay \
+#     IconPackFilledSystemUIOverlay \
+#     IconPackFilledThemePickerOverlay \
+#     IconPackPUIAndroidOverlay \
+#     IconPackPUILauncherOverlay \
+#     IconPackPUISettingsOverlay \
+#     IconPackPUISystemUIOverlay \
+#     IconPackPUIThemePickerOverlay \
+#     IconPackRoundedAndroidOverlay \
+#     IconPackRoundedLauncherOverlay \
+#     IconPackRoundedSettingsOverlay \
+#     IconPackRoundedSystemUIOverlay \
+#     IconPackRoundedThemePickerOverlay \
+#     IconPackOOSAndroidOverlay \
+#     IconPackOOSLauncherOverlay \
+#     IconPackOOSSettingsOverlay \
+#     IconPackOOSSystemUIOverlay \
+#     IconPackOOSThemePickerOverlay \
+#     IconPackOutlineAndroidOverlay \
+#     IconPackOutlineLauncherOverlay \
+#     IconPackOutlineSettingsOverlay \
+#     IconPackOutlineSystemUIOverlay \
+#     IconPackAcherusAndroidOverlay \
+#     IconPackAcherusLauncherOverlay \
+#     IconPackAcherusSettingsOverlay \
+#     IconPackAcherusSystemUIOverlay \
+#     IconPackAuroraAndroidOverlay \
+#     IconPackAuroraSystemUIOverlay \
+#     IconPackGradiconAndroidOverlay \
+#     IconPackGradiconSystemUIOverlay \
+#     IconPackLornAndroidOverlay \
+#     IconPackLornSystemUIOverlay \
+#     IconPackPlumpySystemUIOverlay \
+#     IconPackPlumpyAndroidOverlay \
+#     IconPackXperiaAndroidOverlay \
+#     IconPackXperiaSettingsOverlay \
+#     IconPackXperiaSystemUIOverlay
 
-# Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    vendor/custom/sepolicy/private
+# # Sepolicy
+# SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#     vendor/custom/sepolicy/private
 
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    vendor/custom/sepolicy/vendor
+# BOARD_VENDOR_SEPOLICY_DIRS += \
+#     vendor/custom/sepolicy/vendor
